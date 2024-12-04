f921610347fd9489d18b0d3e4e40a3e9d6ac12a6
achraf <achraf.tata2004@gmail.com>
2024-12-04 19:56:08 +0100

    add the IMetier interface and set up the first functionalities of the project

---
+++ src/main/java/team/project/faceaccess/metier/IMetier.java
@@ -0,0 +1,23 @@
+package team.project.faceaccess.metier;
+
+import lombok.extern.java.Log;
+import team.project.faceaccess.models.AccessLog;
+import team.project.faceaccess.models.User;
+
+import java.util.List;
+
+public interface IMetier {
+    List<User> getAllUsers();
+    List<User> getUsersByKeyword(String keyword);
+    Void changePassword(String oldPassword, String newPassword);
+    void deleteUser(int id);
+    void addUser(User user);
+    void updateUser(User user);
+    void generateRaport(User user);
+    void manageAccess(User user);
+    void viewStatistics(User user);
+    void viewGeneralStatistics();
+    void addLog(AccessLog log);
+    void deleteLog(AccessLog log);
+    List<AccessLog> getLogs();
+}
---
+++ src/main/java/team/project/faceaccess/metier/IMetierImp.java
@@ -0,0 +1,4 @@
+package team.project.faceaccess.metier;
+
+public class IMetierImp {
+}
