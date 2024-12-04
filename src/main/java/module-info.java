66d2bd6d404008dd159690b295bca49b1ac33b84
khemisse-radwane <khemisse.radwane@gmail.com>
2024-12-04 13:50:39 +0100

    Update README

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module team.project.faceaccess {
     requires javafx.controls;
     requires javafx.fxml;
     requires static lombok;
+    requires java.sql;
 
 
     opens team.project.faceaccess to javafx.fxml;
---
+++ src/main/java/team/project/faceaccess/singleton/SingletonConnexionDB.java
@@ -0,0 +1,23 @@
+package team.project.faceaccess.singleton;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SingletonConnexionDB {
+    private static Connection connection;
+
+    private SingletonConnexionDB() {}
+
+    public static Connection getConnexion() {
+        if (connection == null) {
+            try {
+                String dbUrl = "jdbc:sqlite:faceaccess.db";  // For local SQLite file
+                connection = DriverManager.getConnection(dbUrl);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
