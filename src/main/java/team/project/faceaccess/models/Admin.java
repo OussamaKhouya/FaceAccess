dd6fd4d26808b9ec595da556038b36c6ec465fd6
khemisse-radwane <khemisse.radwane@gmail.com>
2024-12-13 16:13:45 +0100

    Creating the faceaccess.db and updating the db path on the connection method

--- src/main/java/team/project/faceaccess/models/Admin.java
+++ src/main/java/team/project/faceaccess/models/Admin.java
@@ -2,9 +2,14 @@ package team.project.faceaccess.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.io.File;
+import java.util.List;
 @Data
 @NoArgsConstructor @AllArgsConstructor
-public class Admin {
-    private int id;
-    private String name;
+public class Admin extends User {
+    private String role = "Admin";
+
+    public Admin(int id, String name, List<File> photos, boolean accessStatus) {
+        super(id, name, photos, accessStatus);
+    }
 }
