2ec2c5e5eabe994173dafe792de4c68264939542
achraf <achraf.tata2004@gmail.com>
2024-12-15 01:41:02 +0100

    welcome view + logo

--- src/main/java/team/project/faceaccess/HelloApplication.java
+++ src/main/java/team/project/faceaccess/HelloApplication.java
@@ -1,20 +1,24 @@
 package team.project.faceaccess;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/welcom-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 399, 315);
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
+
     }
 
     public static void main(String[] args) {
