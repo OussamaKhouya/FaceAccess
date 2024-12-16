17cc41faa2e518e5c72c92d3392b9da7c9eb5b88
khemisse-radwane <khemisse.radwane@gmail.com>
2024-12-16 16:40:52 +0100

    Display Logs with filtered door and user and in a specific range(date)

--- src/main/java/team/project/faceaccess/presentation/AttendanceManagementView.java
+++ src/main/java/team/project/faceaccess/presentation/AttendanceManagementView.java
@@ -7,22 +7,29 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTreeCell;
 import team.project.faceaccess.metier.IMetier;
 import team.project.faceaccess.metier.IMetierImp;
 import team.project.faceaccess.models.AccessLog;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
 import team.project.faceaccess.models.User;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class AttendanceManagementView implements Initializable {
+
+
+    @FXML
+    private DatePicker fromDatePicker;
+
+    @FXML
+    private DatePicker toDatePicker;
 
     @FXML
     private TableView<AccessLog> logTable;
@@ -67,7 +74,12 @@ public class AttendanceManagementView implements Initializable {
 
         // Load logs
         loadLogs();
+        // Load Door Tree
         loadRoomTree();
+
+        // Add listeners to the DatePickers
+        fromDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> filterLogsByDateRange());
+        toDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> filterLogsByDateRange());
     }
 
     private void loadLogs() {
@@ -76,9 +88,12 @@ public class AttendanceManagementView implements Initializable {
     }
 
     private void loadRoomTree() {
-        // Root node for the TreeView
-        TreeItem<String> root = new TreeItem<>("Doors");
-        root.setExpanded(false);
+        // Root node for the TreeView with a checkbox
+        CheckBoxTreeItem<String> root = new CheckBoxTreeItem<>("Doors");
+        root.setExpanded(true);
+        root.setSelected(true);
+
+
 
         // Fetch all doors from the metier layer
         List<String> doors = null;
@@ -87,10 +102,18 @@ public class AttendanceManagementView implements Initializable {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
         if (doors != null) {
             for (String door : doors) {
-                // Create a TreeItem for each door
-                TreeItem<String> doorItem = new TreeItem<>("Door " + door);
+                // Create a CheckBoxTreeItem for each door
+                CheckBoxTreeItem<String> doorItem = new CheckBoxTreeItem<>(door);
+                doorItem.setExpanded(true);
+                doorItem.setSelected(true);
+                // Add listener to doorItem
+                doorItem.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                    filterLogsBasedOnSelection();
+                });
+
 
                 try {
                     // Fetch all users associated with the door
@@ -98,8 +121,17 @@ public class AttendanceManagementView implements Initializable {
 
                     if (users != null && !users.isEmpty()) {
                         for (User user : users) {
-                            // Add each user as a child of the door
-                            TreeItem<String> userItem = new TreeItem<>("-- " + user.getFirstName() + " " + user.getLastName());
+                            // Add each user as a child of the door with a checkbox
+                            CheckBoxTreeItem<String> userItem = new CheckBoxTreeItem<>(
+                                    user.getFirstName() + " " + user.getLastName()
+                            );
+                            userItem.setSelected(true);
+
+                            // Add listener to userItem
+                            userItem.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                                filterLogsBasedOnSelection();
+                            });
+
                             doorItem.getChildren().add(userItem);
                         }
                     } else {
@@ -111,15 +143,98 @@ public class AttendanceManagementView implements Initializable {
                     e.printStackTrace();
                 }
 
-                // Add the door TreeItem to the root
+                // Add the door CheckBoxTreeItem to the root
                 root.getChildren().add(doorItem);
             }
         } else {
             System.err.println("No doors found.");
         }
 
-        // Set the root of the TreeView
+
         roomTree.setRoot(root);
+        // Set the cell factory to display checkboxes
+        roomTree.setCellFactory(CheckBoxTreeCell.forTreeView());
+        // Set the root of the TreeView
+        //roomTree.setRoot(root);
+    }
+
+    // Find the List of selected Items
+    private List<String> getSelectedUsers(CheckBoxTreeItem<String> root) {
+        List<String> selectedUsers = new ArrayList<>();
+
+        // Traverse the tree and collect selected users
+        collectSelectedUsers(root, selectedUsers);
+
+        return selectedUsers;
+    }
+
+    private void collectSelectedUsers(CheckBoxTreeItem<String> item, List<String> selectedUsers) {
+        if (item.isSelected()) {
+            if (item.getChildren().isEmpty()) {
+                // If it's a leaf node (user), add it to the list
+                selectedUsers.add(item.getValue());
+            } else {
+                // If it's a door, add all child users
+                for (TreeItem<String> child : item.getChildren()) {
+                    collectSelectedUsers((CheckBoxTreeItem<String>) child, selectedUsers);
+                }
+            }
+        } else {
+            // Even if a door isn't selected, traverse its children
+            for (TreeItem<String> child : item.getChildren()) {
+                collectSelectedUsers((CheckBoxTreeItem<String>) child, selectedUsers);
+            }
+        }
+    }
+
+
+    // Method to Filter and Update Logs
+    private void filterLogsBasedOnSelection() {
+        // Get the root node of the TreeView
+        CheckBoxTreeItem<String> root = (CheckBoxTreeItem<String>) roomTree.getRoot();
+
+        // Collect all selected users
+        List<String> selectedUsers = getSelectedUsers(root);
+
+        // Filter logs based on selected users
+        ObservableList<AccessLog> filteredLogs = FXCollections.observableArrayList();
+
+        for (AccessLog log : logs) {
+            String userFullName = log.getUser().getFirstName() + " " + log.getUser().getLastName();
+
+            if (selectedUsers.contains(userFullName)) {
+                filteredLogs.add(log);
+            }
+        }
+
+        // Update the TableView with the filtered logs
+        logTable.setItems(filteredLogs);
+    }
+
+
+    private void filterLogsByDateRange() {
+        LocalDate fromDate = fromDatePicker.getValue();
+        LocalDate toDate = toDatePicker.getValue();
+
+        if (fromDate == null && toDate == null) {
+            // If no dates are selected, show all logs
+            logTable.setItems(logs);
+            return;
+        }
+
+        ObservableList<AccessLog> filteredLogs = logs.filtered(log -> {
+            LocalDate logDate = log.getTimestamp().toLocalDate();
+            if (fromDate != null && toDate != null) {
+                return (logDate.isEqual(fromDate) || logDate.isAfter(fromDate))
+                        && (logDate.isEqual(toDate) || logDate.isBefore(toDate));
+            } else if (fromDate != null) {
+                return logDate.isEqual(fromDate) || logDate.isAfter(fromDate);
+            } else { // toDate != null
+                return logDate.isEqual(toDate) || logDate.isBefore(toDate);
+            }
+        });
+
+        logTable.setItems(filteredLogs);
     }
 
 
